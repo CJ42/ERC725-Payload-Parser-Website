7be812f4962a5f5cb884af3bfa9a5837ea84511a
CJ42 <cavallerajean@gmail.com>
2021-10-27 13:28:04 +0100

    fix build errors

--- src/Decode.js
+++ src/Decode.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
-import LaunchIcon from '@material-ui/icons/Launch';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
+import LaunchIcon from "@material-ui/icons/Launch";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 // Layout
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 
 // Components
-import Alert from '@material-ui/lab/Alert';
-import Chip from '@material-ui/core/Chip';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import TextField from '@material-ui/core/TextField';
+import Alert from "@material-ui/lab/Alert";
+import Chip from "@material-ui/core/Chip";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import TextField from "@material-ui/core/TextField";
 
 const SELECTORS = {
   SETDATA: "14a6e293",
   EXECUTE: "44c028fe",
-  TRANSFEROWNERSHIP: "f2fde38b"
-}
+  TRANSFEROWNERSHIP: "f2fde38b",
+};
 
 function ShowDecoder({ selector, payload, web3 }) {
   if (selector === SELECTORS.SETDATA) {
@@ -28,162 +28,183 @@ function ShowDecoder({ selector, payload, web3 }) {
   }
 
   if (selector === SELECTORS.EXECUTE) {
-    return DecodeExecute(payload, web3)
+    return DecodeExecute(payload, web3);
   }
 
   if (selector === SELECTORS.TRANSFEROWNERSHIP) {
-    return DecodeTransferOwnership(payload, web3)
+    return DecodeTransferOwnership(payload, web3);
   }
 
-  return "could not decode payload"
+  return "could not decode payload";
 }
 
 export default function Decode(props) {
   const defaultChips = {
-    "setData": "default",
-    "execute": "default",
-    "transferOwnership": "default",
-  }
+    setData: "default",
+    execute: "default",
+    transferOwnership: "default",
+  };
 
   const [selector, setSelector] = useState("");
-  const [payload, setPayload] = useState("")
+  const [payload, setPayload] = useState("");
   const [chipsBackground, setChipsBackground] = useState(defaultChips);
-  const [abiError, setABIError] = useState({ error: false, message: "" })
+  const [abiError, setABIError] = useState({ error: false, message: "" });
 
   const showFunction = (selector) => {
     // always start with the default chips
-    let newChipsBackground = defaultChips
+    let newChipsBackground = defaultChips;
 
     // find which selector it is + edit the background in the property
-    switch(selector) {
+    switch (selector) {
       case SELECTORS.SETDATA:
-          newChipsBackground.setData = "primary"
-          break;
+        newChipsBackground.setData = "primary";
+        break;
       case SELECTORS.EXECUTE:
-          newChipsBackground.execute = "primary"
-          break;
+        newChipsBackground.execute = "primary";
+        break;
       case SELECTORS.TRANSFEROWNERSHIP:
-          newChipsBackground.transferOwnership = "primary"
-          break;
+        newChipsBackground.transferOwnership = "primary";
+        break;
+      default:
+        break;
     }
     // update the state
-    setChipsBackground(newChipsBackground)
-  }
+    setChipsBackground(newChipsBackground);
+  };
 
   return (
-      <div>
-          <TextareaAutosize 
-            width={900}
-            minRows={8}
-            maxRows={8}
-            placeholder="Paste your abi here..." 
-            onChange={(event) => {
-              let input = event.target.value;
+    <div>
+      <TextareaAutosize
+        width={900}
+        minRows={8}
+        maxRows={8}
+        placeholder="Paste your abi here..."
+        onChange={(event) => {
+          let input = event.target.value;
 
-              // check that the input start with 0x
-              if (input.slice(0, 2) != "0x") {
-                setABIError({ error: true, message: "Invalid payload. Missing `0x` prefix for hexadecimal" })
-                return;
-              }
-
-              // get the first 4 bytes
-              let selector = input.slice(2, 10)
-              let payload = input.slice(10)
-
-              setSelector(selector)
-              setPayload(payload)
-
-              
-              if (!Object.values(SELECTORS).includes(selector)) {
-                setABIError({ error: true, message: "Unrecognised ERC725 selector" })
-                return;
-              } else {
-                setABIError({ error: false, message: "" })
-              }
-                
-              showFunction(selector)
-            }}
-          />
-          {
-            abiError.error 
-              ? <Alert severity="error">{abiError.message}</Alert> 
-              : ""
+          // check that the input start with 0x
+          if (input.slice(0, 2) !== "0x") {
+            setABIError({
+              error: true,
+              message: "Invalid payload. Missing `0x` prefix for hexadecimal",
+            });
+            return;
           }
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={3}>
-                <Grid
-                  container
-                  direction="column"
-                  justifyContent="center"
-                  alignItems="center"
-                  spacing="3"
-                >
-                  <Chip icon={<PlaylistAddIcon />} label="Set Data" color={chipsBackground.setData} />
-                  <Chip icon={<LaunchIcon />} label="Execute" color={chipsBackground.execute} />
-                  <Chip icon={<AccountCircleIcon />} label="Transfer Ownership" color={chipsBackground.transferOwnership} />
-                </Grid>
-              </Grid>
-              <Grid item xs={9}>
-                {
-                  !abiError.error && payload.length > 0
-                    ? <ShowDecoder selector={selector} payload={payload} web3={props.web3} />
-                    : ""
-                }
-              </Grid>
+
+          // get the first 4 bytes
+          let selector = input.slice(2, 10);
+          let payload = input.slice(10);
+
+          setSelector(selector);
+          setPayload(payload);
+
+          if (!Object.values(SELECTORS).includes(selector)) {
+            setABIError({
+              error: true,
+              message: "Unrecognised ERC725 selector",
+            });
+            return;
+          } else {
+            setABIError({ error: false, message: "" });
+          }
+
+          showFunction(selector);
+        }}
+      />
+      {abiError.error ? <Alert severity="error">{abiError.message}</Alert> : ""}
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing="3"
+            >
+              <Chip
+                icon={<PlaylistAddIcon />}
+                label="Set Data"
+                color={chipsBackground.setData}
+              />
+              <Chip
+                icon={<LaunchIcon />}
+                label="Execute"
+                color={chipsBackground.execute}
+              />
+              <Chip
+                icon={<AccountCircleIcon />}
+                label="Transfer Ownership"
+                color={chipsBackground.transferOwnership}
+              />
             </Grid>
-          </Box>
-      </div>
-  )
+          </Grid>
+          <Grid item xs={9}>
+            {!abiError.error && payload.length > 0 ? (
+              <ShowDecoder
+                selector={selector}
+                payload={payload}
+                web3={props.web3}
+              />
+            ) : (
+              ""
+            )}
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
 
 function DecodeSetData(payload, web3) {
   try {
-    let result = web3.eth.abi.decodeParameters(["bytes32[]", "bytes[]"], payload)
+    let result = web3.eth.abi.decodeParameters(
+      ["bytes32[]", "bytes[]"],
+      payload
+    );
     return (
       <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <div>
-              <List dense={true}>
-                  <ListItem>
-                    <b>Keys</b>
-                  </ListItem>
-                  {result[0].map(key => (
-                    <ListItem style={{ fontSize: '10px' }}>
-                      {key}
-                    </ListItem>
-                  ))}
-              </List>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <div>
-              <List dense={true}>
-                <ListItem>
-                  <b>Values</b>
-                </ListItem>
-                {result[1].map(value => (
-                    <ListItem style={{ fontSize: '10px' }}>
-                      {value}
-                    </ListItem>
-                  ))}
-              </List>
-            </div>
-          </Grid>
+        <Grid item xs={12} md={6}>
+          <div>
+            <List dense={true}>
+              <ListItem>
+                <b>Keys</b>
+              </ListItem>
+              {result[0].map((key) => (
+                <ListItem style={{ fontSize: "10px" }}>{key}</ListItem>
+              ))}
+            </List>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <div>
+            <List dense={true}>
+              <ListItem>
+                <b>Values</b>
+              </ListItem>
+              {result[1].map((value) => (
+                <ListItem style={{ fontSize: "10px" }}>{value}</ListItem>
+              ))}
+            </List>
+          </div>
+        </Grid>
       </Grid>
-    )
+    );
   } catch (error) {
     return (
       <Grid item md={12}>
         <DecoderError web3Error={error.message} />
       </Grid>
-    )
+    );
   }
 }
 
 function DecodeExecute(payload, web3) {
   try {
-    let result = web3.eth.abi.decodeParameters(["uint256", "address", "uint256", "bytes"], payload)
+    let result = web3.eth.abi.decodeParameters(
+      ["uint256", "address", "uint256", "bytes"],
+      payload
+    );
     return (
       <Grid container>
         <Grid item md={12}>
@@ -223,48 +244,49 @@ function DecodeExecute(payload, web3) {
           />
         </Grid>
       </Grid>
-    )
+    );
   } catch (error) {
     return (
       <Grid item md={12}>
         <DecoderError web3Error={error.message} />
       </Grid>
-    )
+    );
   }
 }
 
 function DecodeTransferOwnership(payload, web3) {
   try {
-    let result = web3.eth.abi.decodeParameters(["address"], payload)
+    let result = web3.eth.abi.decodeParameters(["address"], payload);
     return (
       <Grid item md={12}>
         <Alert severity="warning">
           <b>Warning! </b>
-          This payload transfer ownership to {result[0]}.<br/> Be cautious!
+          This payload transfer ownership to {result[0]}.<br /> Be cautious!
         </Alert>
         <TextField
-            label="new owner"
-            defaultValue="0x..."
-            value={result[0]}
-            fullWidth
-            InputProps={{ readOnly: true }}
-          />
+          label="new owner"
+          defaultValue="0x..."
+          value={result[0]}
+          fullWidth
+          InputProps={{ readOnly: true }}
+        />
       </Grid>
-    )
+    );
   } catch (error) {
     return (
       <Grid item md={12}>
         <DecoderError web3Error={error.message} />
       </Grid>
-    )
+    );
   }
 }
 
 function DecoderError({ web3Error }) {
   return (
     <Alert severity="error">
-        <b>Web3 ABI Decoder Error! </b><br/>
-        { web3Error }
+      <b>Web3 ABI Decoder Error! </b>
+      <br />
+      {web3Error}
     </Alert>
-  )
+  );
 }
--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
