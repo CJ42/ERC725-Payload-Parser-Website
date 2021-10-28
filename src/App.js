329f628e946dc64e08768db40d9b6c7ebb9769eb
CJ42 <cavallerajean@gmail.com>
2021-10-28 01:27:31 +0100

    Remove SimpleContractStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./abis/SimpleStorage.json";
 import getWeb3 from "./hooks/getWeb3";
 
 import "./App.css";
@@ -9,7 +8,7 @@ import Header from "./components/Header/Header"
 import Body from "./Body"
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0, web3: null, accounts: null /*, contract: null */ };
 
   componentDidMount = async () => {
     try {
@@ -18,17 +17,9 @@ class App extends Component {
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ web3, accounts });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
